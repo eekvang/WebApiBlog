a2c536beac2086aadb5d11670b212f800752c2e3
Espen Ekvang <eekvang@gmail.com>
2012-08-16 08:34:28 +0200

    Added WebApiConsole app to illustrate how to consume ASP.NET Web API

---
+++ WebApiConsole/WebApiConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace WebApiConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var blogEntries = new WebApiAdapter(args[0]).Get();
+
+            foreach (var blogEntry in blogEntries)
+            {
+                Console.WriteLine(blogEntry.Title);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ WebApiConsole/WebApiConsole/WebApiAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace WebApiConsole
+{
+    public class WebApiAdapter
+    {
+        private readonly string _url;
+
+        public WebApiAdapter(string url)
+        {
+            _url = url;
+        }
+
+        public IEnumerable<BlogEntry> Get()
+        {
+            var result = new HttpClient().GetAsync(_url).Result;
+            return JsonConvert.DeserializeObject<IEnumerable<BlogEntry>>(result.Content.ReadAsStringAsync().Result);
+        }
+    }
+
+    public class BlogEntry
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
